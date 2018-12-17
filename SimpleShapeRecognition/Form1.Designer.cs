f90195e755e58f15bfa8e9c513a576de0e46ff32
Nakido <yuyukosaigyouji@foxmail.com>
2018-12-17 10:53:41 +0800

    Test

--- SimpleShapeRecognition/Form1.Designer.cs
+++ SimpleShapeRecognition/Form1.Designer.cs
@@ -23,9 +23,17 @@
 		/// 使用代码编辑器修改此方法的内容。
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.SuspendLayout();
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(617, 346);
+			this.Name = "Form1";
+			this.Text = "SimpleShapeRecognition";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
