323933d55b0614b6d1b2368d3d4593531becc24f
Nakido <yuyukosaigyouji@foxmail.com>
2019-01-25 00:04:31 +0800

    nakido 19-1-25.1

--- MyGIS/FormMain.Designer.cs
+++ MyGIS/FormMain.Designer.cs
@@ -30,18 +30,39 @@
 			this.map1 = new DotSpatial.Controls.Map();
 			this.menuStrip = new System.Windows.Forms.MenuStrip();
 			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
 			this.openShapefileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.shapefileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.gridToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.selectAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.selectNoneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.reverseSelectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
 			this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.toggleDebugLogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.layersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.mapRenderingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolBarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.statusBarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.procToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.basicOperationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.landformIdentificationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.lineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.circleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.crescentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
 			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toggleDebugLogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.debugToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.dotSpatialTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.sSRTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -50,6 +71,9 @@
 			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
 			this.statusStrip = new System.Windows.Forms.StatusStrip();
 			this.statusBar = new System.Windows.Forms.ToolStripStatusLabel();
+			this.onlineHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+			this.debuggingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolStripContainer.ContentPanel.SuspendLayout();
 			this.toolStripContainer.TopToolStripPanel.SuspendLayout();
 			this.toolStripContainer.SuspendLayout();
@@ -142,19 +166,45 @@
 			// fileToolStripMenuItem
 			// 
 			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator3,
             this.openShapefileToolStripMenuItem,
+            this.exportToolStripMenuItem,
             this.toolStripSeparator1,
+            this.propertiesToolStripMenuItem,
             this.exitToolStripMenuItem});
 			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
 			resources.ApplyResources(this.fileToolStripMenuItem, "fileToolStripMenuItem");
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			resources.ApplyResources(this.openToolStripMenuItem, "openToolStripMenuItem");
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			resources.ApplyResources(this.saveToolStripMenuItem, "saveToolStripMenuItem");
+			// 
+			// saveAsToolStripMenuItem
+			// 
+			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+			resources.ApplyResources(this.saveAsToolStripMenuItem, "saveAsToolStripMenuItem");
+			// 
+			// toolStripSeparator3
+			// 
+			this.toolStripSeparator3.Name = "toolStripSeparator3";
+			resources.ApplyResources(this.toolStripSeparator3, "toolStripSeparator3");
 			// 
 			// openShapefileToolStripMenuItem
 			// 
 			this.openShapefileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.shapefileToolStripMenuItem,
             this.gridToolStripMenuItem});
-			resources.ApplyResources(this.openShapefileToolStripMenuItem, "openShapefileToolStripMenuItem");
 			this.openShapefileToolStripMenuItem.Name = "openShapefileToolStripMenuItem";
+			resources.ApplyResources(this.openShapefileToolStripMenuItem, "openShapefileToolStripMenuItem");
 			// 
 			// shapefileToolStripMenuItem
 			// 
@@ -166,10 +216,20 @@
 			this.gridToolStripMenuItem.Name = "gridToolStripMenuItem";
 			resources.ApplyResources(this.gridToolStripMenuItem, "gridToolStripMenuItem");
 			// 
+			// exportToolStripMenuItem
+			// 
+			this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+			resources.ApplyResources(this.exportToolStripMenuItem, "exportToolStripMenuItem");
+			// 
 			// toolStripSeparator1
 			// 
 			this.toolStripSeparator1.Name = "toolStripSeparator1";
 			resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
+			// 
+			// propertiesToolStripMenuItem
+			// 
+			this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+			resources.ApplyResources(this.propertiesToolStripMenuItem, "propertiesToolStripMenuItem");
 			// 
 			// exitToolStripMenuItem
 			// 
@@ -180,9 +240,33 @@
 			// editToolStripMenuItem
 			// 
 			this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectAllToolStripMenuItem,
+            this.selectNoneToolStripMenuItem,
+            this.reverseSelectionToolStripMenuItem,
+            this.toolStripSeparator2,
             this.optionsToolStripMenuItem});
 			this.editToolStripMenuItem.Name = "editToolStripMenuItem";
 			resources.ApplyResources(this.editToolStripMenuItem, "editToolStripMenuItem");
+			// 
+			// selectAllToolStripMenuItem
+			// 
+			this.selectAllToolStripMenuItem.Name = "selectAllToolStripMenuItem";
+			resources.ApplyResources(this.selectAllToolStripMenuItem, "selectAllToolStripMenuItem");
+			// 
+			// selectNoneToolStripMenuItem
+			// 
+			this.selectNoneToolStripMenuItem.Name = "selectNoneToolStripMenuItem";
+			resources.ApplyResources(this.selectNoneToolStripMenuItem, "selectNoneToolStripMenuItem");
+			// 
+			// reverseSelectionToolStripMenuItem
+			// 
+			this.reverseSelectionToolStripMenuItem.Name = "reverseSelectionToolStripMenuItem";
+			resources.ApplyResources(this.reverseSelectionToolStripMenuItem, "reverseSelectionToolStripMenuItem");
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			resources.ApplyResources(this.toolStripSeparator2, "toolStripSeparator2");
 			// 
 			// optionsToolStripMenuItem
 			// 
@@ -193,33 +277,112 @@
 			// viewToolStripMenuItem
 			// 
 			this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toggleDebugLogToolStripMenuItem});
+            this.layersToolStripMenuItem,
+            this.mapRenderingToolStripMenuItem,
+            this.toolBarToolStripMenuItem,
+            this.statusBarToolStripMenuItem});
 			this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
 			resources.ApplyResources(this.viewToolStripMenuItem, "viewToolStripMenuItem");
 			// 
-			// toggleDebugLogToolStripMenuItem
+			// layersToolStripMenuItem
 			// 
-			this.toggleDebugLogToolStripMenuItem.Name = "toggleDebugLogToolStripMenuItem";
-			resources.ApplyResources(this.toggleDebugLogToolStripMenuItem, "toggleDebugLogToolStripMenuItem");
-			this.toggleDebugLogToolStripMenuItem.Click += new System.EventHandler(this.toggleDebugLogToolStripMenuItem_Click);
+			this.layersToolStripMenuItem.Checked = true;
+			this.layersToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.layersToolStripMenuItem.Name = "layersToolStripMenuItem";
+			resources.ApplyResources(this.layersToolStripMenuItem, "layersToolStripMenuItem");
+			// 
+			// mapRenderingToolStripMenuItem
+			// 
+			this.mapRenderingToolStripMenuItem.Checked = true;
+			this.mapRenderingToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.mapRenderingToolStripMenuItem.Name = "mapRenderingToolStripMenuItem";
+			resources.ApplyResources(this.mapRenderingToolStripMenuItem, "mapRenderingToolStripMenuItem");
+			// 
+			// toolBarToolStripMenuItem
+			// 
+			this.toolBarToolStripMenuItem.Checked = true;
+			this.toolBarToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.toolBarToolStripMenuItem.Name = "toolBarToolStripMenuItem";
+			resources.ApplyResources(this.toolBarToolStripMenuItem, "toolBarToolStripMenuItem");
+			// 
+			// statusBarToolStripMenuItem
+			// 
+			this.statusBarToolStripMenuItem.Checked = true;
+			this.statusBarToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.statusBarToolStripMenuItem.Name = "statusBarToolStripMenuItem";
+			resources.ApplyResources(this.statusBarToolStripMenuItem, "statusBarToolStripMenuItem");
 			// 
 			// procToolStripMenuItem
 			// 
+			this.procToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.basicOperationsToolStripMenuItem,
+            this.landformIdentificationToolStripMenuItem});
 			this.procToolStripMenuItem.Name = "procToolStripMenuItem";
 			resources.ApplyResources(this.procToolStripMenuItem, "procToolStripMenuItem");
+			// 
+			// basicOperationsToolStripMenuItem
+			// 
+			this.basicOperationsToolStripMenuItem.Name = "basicOperationsToolStripMenuItem";
+			resources.ApplyResources(this.basicOperationsToolStripMenuItem, "basicOperationsToolStripMenuItem");
+			// 
+			// landformIdentificationToolStripMenuItem
+			// 
+			this.landformIdentificationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lineToolStripMenuItem,
+            this.circleToolStripMenuItem,
+            this.crescentToolStripMenuItem});
+			this.landformIdentificationToolStripMenuItem.Name = "landformIdentificationToolStripMenuItem";
+			resources.ApplyResources(this.landformIdentificationToolStripMenuItem, "landformIdentificationToolStripMenuItem");
+			// 
+			// lineToolStripMenuItem
+			// 
+			this.lineToolStripMenuItem.Name = "lineToolStripMenuItem";
+			resources.ApplyResources(this.lineToolStripMenuItem, "lineToolStripMenuItem");
+			// 
+			// circleToolStripMenuItem
+			// 
+			this.circleToolStripMenuItem.Name = "circleToolStripMenuItem";
+			resources.ApplyResources(this.circleToolStripMenuItem, "circleToolStripMenuItem");
+			// 
+			// crescentToolStripMenuItem
+			// 
+			this.crescentToolStripMenuItem.Name = "crescentToolStripMenuItem";
+			resources.ApplyResources(this.crescentToolStripMenuItem, "crescentToolStripMenuItem");
 			// 
 			// helpToolStripMenuItem
 			// 
 			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem1,
+            this.onlineHelpToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.aboutToolStripMenuItem,
+            this.toolStripSeparator5,
+            this.debuggingToolStripMenuItem,
+            this.toggleDebugLogToolStripMenuItem});
 			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
 			resources.ApplyResources(this.helpToolStripMenuItem, "helpToolStripMenuItem");
+			// 
+			// helpToolStripMenuItem1
+			// 
+			this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+			resources.ApplyResources(this.helpToolStripMenuItem1, "helpToolStripMenuItem1");
+			// 
+			// toolStripSeparator4
+			// 
+			this.toolStripSeparator4.Name = "toolStripSeparator4";
+			resources.ApplyResources(this.toolStripSeparator4, "toolStripSeparator4");
 			// 
 			// aboutToolStripMenuItem
 			// 
 			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
 			resources.ApplyResources(this.aboutToolStripMenuItem, "aboutToolStripMenuItem");
 			this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+			// 
+			// toggleDebugLogToolStripMenuItem
+			// 
+			this.toggleDebugLogToolStripMenuItem.Name = "toggleDebugLogToolStripMenuItem";
+			resources.ApplyResources(this.toggleDebugLogToolStripMenuItem, "toggleDebugLogToolStripMenuItem");
+			this.toggleDebugLogToolStripMenuItem.Click += new System.EventHandler(this.toggleDebugLogToolStripMenuItem_Click);
 			// 
 			// debugToolStripMenuItem
 			// 
@@ -276,6 +439,21 @@
 			this.statusBar.Name = "statusBar";
 			resources.ApplyResources(this.statusBar, "statusBar");
 			// 
+			// onlineHelpToolStripMenuItem
+			// 
+			this.onlineHelpToolStripMenuItem.Name = "onlineHelpToolStripMenuItem";
+			resources.ApplyResources(this.onlineHelpToolStripMenuItem, "onlineHelpToolStripMenuItem");
+			// 
+			// toolStripSeparator5
+			// 
+			this.toolStripSeparator5.Name = "toolStripSeparator5";
+			resources.ApplyResources(this.toolStripSeparator5, "toolStripSeparator5");
+			// 
+			// debuggingToolStripMenuItem
+			// 
+			resources.ApplyResources(this.debuggingToolStripMenuItem, "debuggingToolStripMenuItem");
+			this.debuggingToolStripMenuItem.Name = "debuggingToolStripMenuItem";
+			// 
 			// FormMain
 			// 
 			resources.ApplyResources(this, "$this");
@@ -330,9 +508,33 @@
 		private System.Windows.Forms.ToolStripMenuItem sSRTestToolStripMenuItem;
 		private System.Windows.Forms.ToolStripContainer toolStripContainer;
 		private System.Windows.Forms.ToolStripStatusLabel statusBar;
-		private System.Windows.Forms.ToolStripMenuItem toggleDebugLogToolStripMenuItem;
 		private DotSpatial.Controls.Legend legend1;
 		private System.Windows.Forms.ToolStripMenuItem sSREnumToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+		private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem selectAllToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem selectNoneToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem reverseSelectionToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+		private System.Windows.Forms.ToolStripMenuItem basicOperationsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem landformIdentificationToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem lineToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem circleToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem crescentToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem layersToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem mapRenderingToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem toolBarToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem statusBarToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+		private System.Windows.Forms.ToolStripMenuItem toggleDebugLogToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem onlineHelpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+		private System.Windows.Forms.ToolStripMenuItem debuggingToolStripMenuItem;
 	}
 }
 
