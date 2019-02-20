80d1b56d129834684bdc72adada43f8c59316fd7
Nakido Al. <yuyukosaigyouji@foxmail.com>
2019-02-20 21:20:48 +0800

    nakido 2.20 final2

--- MyGIS.Desktop/Properties/AssemblyInfo.cs
+++ MyGIS.Desktop/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("MyGIS Desktop Alpha")]
+[assembly: AssemblyTitle("MyGIS Desktop Beta")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Team Wuyanzu, School of Geography, NNU")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.*")]
+[assembly: AssemblyVersion("1.5.*")]
 [assembly: AssemblyFileVersion("12.2.6967.34104")]
